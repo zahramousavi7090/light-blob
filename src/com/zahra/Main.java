61a4f8ee7991a62b70ef7e5e819fc727a39c5cdb
Zahra <zahramousavi7090@gmail.com>
2020-11-26 16:37:05 +0330

    use lightBlob

--- src/com/zahra/Main.java
+++ src/com/zahra/Main.java
@@ -3,7 +3,9 @@ package com.zahra;
 public class Main {
 
     public static void main(String[] args) {
-
+        LightBlob lightBlob=new LightBlob();
+        int numsClick = lightBlob.lightBlob(new int[]{6, 5, 4, 3, 1, 2, 7});
+        System.out.println(numsClick);
     }
 
 
